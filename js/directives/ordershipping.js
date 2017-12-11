ae5135039f180b30769be890155983f9ab2ea15a
novermann <novermann@prograde.com>
2017-12-11 13:31:02 -0500

    Residential Address Field
    Setting a custom order field based on if the chosen ship address is a custom address.

--- js/directives/ordershipping.js
+++ js/directives/ordershipping.js
@@ -152,6 +152,26 @@ four51.app.directive('ordershipping', ['Order', 'Shipper', 'Address', 'AddressLi
 					li.Shipper = null;
 					li.ShipperID = null;
 				});	
+				
+				/* Checking for residential address and setting appropriate value */
+				angular.forEach($scope.shipaddresses, function(address) {
+                    angular.forEach($scope.currentOrder.OrderFields, function(field) {
+                        if (field && field.Name == 'VFOUR51_RESIDENT') {
+                            angular.forEach(field.Options, function(option) {
+                                option.Selected = false;
+                                if (address.IsCustEditable && option.Value == 'Y'){
+                                    option.Selected = true;
+                                    field.Value = option.Value;
+                                }
+                                else if (!address.IsCustEditable && option.Value == 'N'){
+                                    option.Selected = true;
+                                    field.Value = option.Value;
+                                }
+                            });
+                        }
+                    });
+				});
+				
 				saveChanges(
 					function(order) {
 						Shipper.query(order, function(list) {
